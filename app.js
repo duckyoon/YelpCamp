561ec11acb5db46f2885ab5369adced269464099
YoonDuck <dkdlel616@gmail.com>
2025-03-12 16:33:43 +0900

    250312_Passport_first

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -47,10 +49,35 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+// passport를 express에서 사용할 수 있도록 초기화 하는 미들웨어
+app.use(passport.initialize());
+
+// 세션을 이용한 로그인 유지 미들웨어 (사용 전 반드시 초기화 해야함)
+// req.session.passport에 사용자 ID 저장
+app.use(passport.session());
+
+// paasport-local을 사용하여 username과 password를 사용하여 로그인하도록 함.
+// findOne(username) 으로 사용자를 찾고 bcrypt.compare로 비밀번호가 맞는지 확인.
+passport.use(new LocalStrategy(User.authenticate()));
+
+// 사용자를 세션에 저장하는 방법 정의(로그인하면 사용자의 ID를 세션에 저장)
+passport.serializeUser(User.serializeUser());
+
+// 세션에서 사용자 정보를 가져오는 방법 정의(serializeUser()에서 저장한 사용자 ID를 기반으로 사용자 정보를 찾아 req.user에 저장)
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser', async(req, res) => {
+    const user = new User({ email: 'test@gamil.com', username: 'test' })
+    // register 사용하여 데이터베이스에 저장(현재는 평문이나 실제로 passport가 hash 해서 저장한다.)
+    const newUser = await User.register(user, 'pass')
+    res.send(newUser);
 })
 
 app.use('/campgrounds', campgrounds)
