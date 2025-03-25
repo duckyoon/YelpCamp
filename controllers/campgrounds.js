268ed0b2b30ee76db246b77973995e7eac7ad8a5
duckyoon <dkdlel616@gmail.com>
2025-03-26 00:14:04 +0900

    Feat: 이미지 삭제 기능 추가

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,6 @@
 const Campground = require('../models/campground');
+const {cloudinary} = require('../cloudinary');
+
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -46,10 +48,18 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.editCampground = async (req, res) => {
     const { id } = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.image.push(...imgs)
     await campground.save()
+    if (req.body.deleteImages && req.body.deleteImages.length > 0){
+        for (let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull:{ image:{filename:{$in: req.body.deleteImages}}}})
+        console.log(campground)
+    }
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 };
